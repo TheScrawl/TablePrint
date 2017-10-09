8edda73396667aee90fd7269de19cd8c0221c234
TheScrawl <the.scrawl.mail@gmail.com>
2017-10-09 21:06:52 +1300

    minor fixes

--- TablePrint.py
+++ TablePrint.py
@@ -14,15 +14,18 @@ def table(*argv):
 	for dataSet in argv:
 		dataSetCount = dataSetCount + 1
 		itemCount = 0
-		print('\n' + '-'*(longestStr * longestData + longestData + 1))
+		if itemCount == 0:
+			print('\n' + '-'*(longestStr * longestData + longestData + len(dataSet) + 1))
+		else:
+			print('-'*(longestStr * longestData + longestData + 1))
 		print('|', end='')
 		for item in dataSet:
 			itemCount = itemCount + 1
 			if itemCount <= len(dataSet):
-				print(str(item) + ' ' * (longestStr - len(str(item))), end = '|')
+				print(str(item) + ' ' * (longestStr - len(str(item)) + 1), end = '|')
 
 		x = len(dataSet)
 		while x < longestData:
 			print(' ' * longestStr, end = '|')
 			x = x + 1
-	print('\n' + '-'*(longestStr * longestData + longestData + 1))
+	print('\n' + '-'*(longestStr * longestData + longestData + len(dataSet) + 1))
